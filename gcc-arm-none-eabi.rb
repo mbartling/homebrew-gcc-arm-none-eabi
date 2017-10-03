c4134abb5b81b27d6295dbee2c9be649ea249eb8
Michael Bartling <michael.bartling@arm.com>
2017-10-03 11:45:27 -0500

    Updat url

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -5,13 +5,11 @@
 class GccArmNoneEabi < Formula
   desc "GCC arm"
   homepage "https://developer.arm.com"
-  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2?revision=ef567310-404c-48e2-8b67-12e3c86f584a?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,6-2017-q2-update"
+  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2"
   sha256 "7d3080514a2899d05fc55466cdc477e2448b6a62f536ffca3dd846822ff52900"
 
   def install
-    system "tar xf *.tar"
-    system "rm *.tar"
-    prefix.install Dir["gcc-arm*/*"]
+    prefix.install Dir["./*"]
   end
 
   test do
